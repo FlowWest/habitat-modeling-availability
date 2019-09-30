cd789b86ef32a5e019b44cdaeaeba3b9ad6d3391
ergz <erodriguez@flowwest.com>
2019-09-30 09:00:06 -0700

    move the species select input to the top of the panel

--- ui.R
+++ ui.R
@@ -5,13 +5,13 @@ shinyUI(fluidPage(
         sidebarPanel(
             width = 3, 
             tags$p("This application summarizes how habitat input data was developed for each watershed in the Science Integration Team (SIT) population models. Red (FALSE) entries indicate that no watershed specific habitat modeling was available for a given species and lifestage. Habitat input data for these watersheds was generated through regional approximation. Green (TRUE) entries indicate that watershed specific habitat modeling was available for at least some portion of the watershed for a given species and lifestage. The Docs link for each watershed provides detailed descriptions of methods used to develop habitat inputs."),
+            selectInput("species", "Select species", 
+                        choices = c("Fall Run", "Spring Run", "Steelhead")),
             selectInput("region", "Select by Region", choices = regions_lookup$Region), 
             tags$p("or"),
             selectizeInput("watershed", "Select by Watersheds",
                            choices = watersheds,
-                           options = list(maxItems = 20)),
-            selectInput("species", "Select species", 
-                        choices = c("Fall Run", "Spring Run", "Steelhead"))
+                           options = list(maxItems = 20))
         ),
 
         mainPanel(
